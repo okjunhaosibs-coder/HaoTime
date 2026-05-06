0431f450fc9bdf0c0f07f9c22d6f9b1d0ecf7d1d
junhao <okjunhaosibs@gmail.com>
2026-05-06 13:09:05 +0800

    feat: switch to CloudKit-enabled modelContainer

--- HaoTime/HaoTimeApp.swift
+++ HaoTime/HaoTimeApp.swift
@@ -3,24 +3,13 @@ import SwiftData
 
 @main
 struct HaoTimeApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Category.self,
-            Session.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(sharedModelContainer)
+        .modelContainer(for: [Category.self, Session.self])
+        #if os(macOS)
+        .windowResizability(.contentMinSize)
+        #endif
     }
 }
