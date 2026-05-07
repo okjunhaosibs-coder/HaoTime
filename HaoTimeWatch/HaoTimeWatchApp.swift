caee30fbe21bd0c2f93839780688c8c37f827df8
junhao <okjunhaosibs@gmail.com>
2026-05-07 20:40:12 +0800

    feat: add watchOS app entry

---
+++ HaoTimeWatch/HaoTimeWatchApp.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+import SwiftData
+
+@main
+struct HaoTimeWatchApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([Category.self, Session.self])
+        let config = ModelConfiguration(
+            schema: schema,
+            isStoredInMemoryOnly: false
+        )
+        do {
+            return try ModelContainer(for: schema, configurations: [config])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    @State private var dataVM = DataViewModel()
+    @State private var timerVM = TimerViewModel()
+
+    var body: some Scene {
+        WindowGroup {
+            WatchMainView(dataVM: dataVM, timerVM: timerVM)
+                .onAppear {
+                    dataVM.fetchCategories(context: sharedModelContainer.mainContext)
+                    dataVM.aggregateForWeek(containing: Date(),
+                        context: sharedModelContainer.mainContext)
+                    WatchConnectivityManager.shared.activate()
+                }
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
